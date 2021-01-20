967502a7d09b51264e75fa64fdd5c29056f4a794
harryyan1238 <harryyan1238@gmail.com>
2021-01-20 22:07:18 +1300

    add java version

--- DataStructure/Array/remove_chars_from_str.py
+++ DataStructure/Array/remove_chars_from_str.py
@@ -31,3 +31,26 @@ def removeChars(str, remove):
 result = removeChars("Battle of the Vowels: Hawaii vs. Grozny", "aeiou")
 
 print(result)
+
+
+######## Java ########
+
+# public static void removeChars( StringBuilder str, String remove ) {
+
+#     boolean[] flags = new boolean['z' + 1]; // assumes ASCII, 123
+#     int src, dst = 0;
+#     // Set flags for characters to be removed
+#     for (char c: remove.toCharArray()) {
+#       flags[c] = true;
+#     }
+#
+#     // Now loop through all the characters,
+#     // copying only if they aren't flagged
+#     for ( src = 0; src < str.length(); ++src ) {
+#         char c = str.charAt(src); if ( !flags[ c ] ) {
+#         str.setCharAt( dst++, c ); }
+#     }
+#     str.setLength(dst);
+#     return;
+
+# }
