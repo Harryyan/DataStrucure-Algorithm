c8e0c3c2f7038cea668f2f0ad6ced6af10f12b7e
harryyan1238 <harryyan1238@gmail.com>
2021-01-27 07:46:19 +1300

    add c implementation in comments

--- DataStructure/Array/reverse.py
+++ DataStructure/Array/reverse.py
@@ -12,3 +12,37 @@ str2 = str.split()[::-1]
 print(id(str))
 print(id(str2))
 print(" ".join(str2))
+
+
+# C Implementation
+
+# void wcReverseString( wchar_t str[], int start, int end ){
+#   wchar_t temp;
+#   while ( end > start ){
+# /* Exchange characters */
+#       temp = str[start];
+#       str[start] = str[end];
+#       str[end] = temp;
+# /* Move indices towards middle */ start++; end--;
+#   }
+# }
+
+
+# void wcReverseWords( wchar_t str[] ){
+#   int start = 0, end = 0, length;
+#   length = wcslen(str);
+# /* Reverse entire string */
+#   wcReverseString(str, start, length - 1);
+#   while ( end < length ) {
+#       if ( str[end] != L' ' ){ /* Skip non-word characters */ /* Save position of beginning of word */
+#           start = end;
+# /* Scan to next non-word character */
+#           while ( end < length && str[end] != L' ' )
+#               end++;
+# /* Back up to end of word */
+#           end--;
+# /* Reverse word */
+#           wcReverseString( str, start, end );
+#       }
+#       end++; /* Advance to next token */ }
+# }
