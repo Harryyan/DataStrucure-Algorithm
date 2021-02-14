64f642cb56eb28a7b6b3f767b64a47508f739f96
harryyan1238 <harryyan1238@gmail.com>
2021-02-14 19:08:18 +1300

    delete node of doubly list in O(1)

--- DS/LinkedList/Algorithms-DeleteNode-DoublyLinkedlist.py
+++ DS/LinkedList/Algorithms-DeleteNode-DoublyLinkedlist.py
@@ -2,8 +2,21 @@ from DoubleLinkedList import DoubleLinkedList
 from Node import Node
 
 
-def deleteNode(node: Node):
-    print("")
+def deleteNode(node: Node, head: Node):
+    # head
+    if node.pre == None:
+        temp = head
+        head = node.next
+        head.pre = None
+        temp = None
+    elif node.next == None:
+        node.pre.next = None
+        node.pre = None
+        node = None
+    else:
+        node.pre.next = node.next
+        node.next.pre = node.pre
+        node = None
 
 
 def print_list(link_list):
@@ -17,5 +30,13 @@ if __name__ == '__main__':
     link_list = DoubleLinkedList()
     for i in range(6):
         link_list.add(i)
+
+    print_list(link_list)
+
+    # 2nd node
+    head = link_list.head()
+    node = link_list.get_node(2)
+    print(head.next.pre)
+    deleteNode(node, head)
 
     print_list(link_list)
--- DS/LinkedList/DoubleLinkedList.py
+++ DS/LinkedList/DoubleLinkedList.py
@@ -5,17 +5,31 @@ class DoubleLinkedList:
     def __init__(self):
         self._head = None
 
+    def head(self):
+        return self._head
+
     def add(self, item):
         node = Node(item)
+
         if self.is_empty():
             self._head = node
         else:
             node.next = self._head
-            self._head.prev = node
+            self._head.pre = node
             self._head = node
 
     def is_empty(self):
         return self._head is None
+
+    def get_node(self, index):
+        i = 0
+        cur = self._head
+
+        while cur != None and i < index:
+            cur = cur.next
+            i += 1
+
+        return cur
 
     def append(self, item):
         node = Node(item)
@@ -25,7 +39,7 @@ class DoubleLinkedList:
             cur = self._head
             while cur.next is not None:
                 cur = cur.next
-            node.prev = cur
+            node.pre = cur
             cur.next = node
 
     def items(self):
