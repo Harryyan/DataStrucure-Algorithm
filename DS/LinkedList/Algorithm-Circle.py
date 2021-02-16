10aaa640a3d4d1f6e6e0d1922f9b5daad36f8bb7
harryyan1238 <harryyan1238@gmail.com>
2021-02-16 23:02:25 +1300

    add multi algorithm

---
+++ DS/LinkedList/Algorithm-Circle.py
@@ -0,0 +1,12 @@
+from SingleLinkedList import SingleLinkedList
+from Node import Node
+
+
+def isCircleLinkedList(list: SingleLinkedList):
+    p1 = p2 = list.head()
+
+
+if __name__ == "__main__":
+    result = isCircleLinkedList()
+
+    print(result)
