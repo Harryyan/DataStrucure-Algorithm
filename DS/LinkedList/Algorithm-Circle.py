ed09413ed242bdbd0e59987f988c47307f1a7ce3
harryyan1238 <harryyan1238@gmail.com>
2021-02-16 23:03:17 +1300

    add comments

--- DS/LinkedList/Algorithm-Circle.py
+++ DS/LinkedList/Algorithm-Circle.py
@@ -1,6 +1,9 @@
 from SingleLinkedList import SingleLinkedList
 from Node import Node
 
+# 输入一个单向链表，判断链表是否有环？
+# 看是否能相遇; walker 和 runner 类型
+
 
 def isCircleLinkedList(list: SingleLinkedList):
     p1 = p2 = list.head()
