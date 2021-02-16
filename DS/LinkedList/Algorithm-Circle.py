88b3433977f26d2de297abc55ff9fb2f4c56e6d7
harryyan1238 <harryyan1238@gmail.com>
2021-02-17 09:03:30 +1300

    find 1st intercept

--- DS/LinkedList/Algorithm-Circle.py
+++ DS/LinkedList/Algorithm-Circle.py
@@ -49,6 +49,67 @@ def findEntryNodeOfCircleList(list: SingleLinkedList):
     return walker
 
 
+# 题目3:
+# 输入2个单向有环链表头指针，如何判断相交.
+# 环上的点总是在两个链表中的, 只要找到一条
+# 链表上环上的一点，再判断该点是否在另一条
+# 上即可；
+
+def isTwoCircleListIntercept(h1: Node, h2: Node):
+    walker = runner = h1
+    result = False
+
+    while walker is not None and runner.next is not None:
+        walker = walker.next
+        runner = runner.next.next
+
+        if walker == runner:
+            break
+
+    while h2 is not None:
+        h2 = h2.next
+
+        if h2 == walker:
+            result = True
+            break
+
+    return result
+
+
+# 题目4:
+# 两个无环单链表相交，怎么求出他们相交的第一个节点呢？
+# 链表长度相减，长的那个先向后移动差值个node，然后
+# 两个链表同时向后一步一步移动
+
+def findFirstInterceptNode(list1: SingleLinkedList, list2: SingleLinkedList):
+    l1 = list1.length()
+    l2 = list2.length()
+
+    p1 = list1.head()
+    p2 = list2.head()
+
+    i = 0
+
+    if l2 > l1:
+        while i < l2 - l1 and p2 is not None:
+            p2 = p2.next
+
+        while p1 != p2:
+            p1 = p1.next
+            p2 = p2.next
+
+        return p1
+    else:
+        while i < l1 - l2 and p1 is not None:
+            p1 = p1.next
+
+        while p1 != p2 and p1 is not None:
+            p1 = p1.next
+            p2 = p2.next
+
+        return p1
+
+
 if __name__ == "__main__":
     result = isCircleLinkedList()
 
