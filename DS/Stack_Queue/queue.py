aa78bd4c0c7b5134f1e7bcf0af00ea2193b2d428
harryyan1238 <harryyan1238@gmail.com>
2021-02-21 09:31:22 +1300

    add queue

---
+++ DS/Stack_Queue/queue.py
@@ -0,0 +1,41 @@
+
+# 对于队列来说，入队和出队总有一个时间复杂度是O(n)
+# 这时候就要看你的操作，是出队操作多还是入队操作多
+# 优化选择
+
+class Queue:
+
+    def __init__(self):
+        self.__content = []
+
+    def enqueue(self, value):
+        self.__content.append(value)
+        #self.__content.insert(0, value)
+
+    def dequeue(self):
+        self.__content.pop(0)
+        # self.__content.pop()
+
+    def size(self):
+        return len(self.__content)
+
+    def is_empyh(self):
+        return not self.__content
+
+    def print(self):
+        print(self.__content)
+
+
+if __name__ == "__main__":
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.enqueue(4)
+    queue.enqueue(5)
+
+    queue.print()
+
+    queue.dequeue()
+
+    queue.print()
