8a19bf216fa9326a5233dffe6aed6d777bd6cdaa
harryyan1238 <harryyan1238@gmail.com>
2021-02-22 17:47:52 +1300

    change to valid value

--- DS/Array:String_Related/StrToInt.java
+++ DS/Array:String_Related/StrToInt.java
@@ -29,7 +29,7 @@ public class StrToInt {
     }
 
     public static void main(String[] args) {
-        int result = StrToInt.strToInt("32345353453453451");
+        int result = StrToInt.strToInt("888888");
 
         System.out.println(result);
     }
