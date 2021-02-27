748152d445253482526117f742078d0a28564197
harryyan1238 <harryyan1238@gmail.com>
2021-02-27 20:30:22 +1300

    add another way of insertion

--- DS/Sort/Insertion_Sort.py
+++ DS/Sort/Insertion_Sort.py
@@ -5,10 +5,19 @@ def insertion_sort(items):
             if items[temp] < items[j]:
                 items[temp], items[j] = items[j], items[temp]
                 temp = j
-            print(items)
     return items
 
 
-items = [88, 83, 10, 9, 8, 7, 5, 5, 4, 3, 2, 1]
+def insertion_sort_1(items):
+    for i in range(1, len(items)):
+        for j in range(0, i):
+            if items[i] < items[j]:
+                temp = items[i]
+                items.pop(i)
+                items.insert(j, temp)
+    return items
+
+
+items = [88, 83, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 print(insertion_sort(items))
