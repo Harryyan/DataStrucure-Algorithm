4a40e6fb63263c7a472b92c4c45792a5abc7e013
harryyan1238 <harryyan1238@gmail.com>
2021-03-21 10:22:58 +1300

    add cache

--- DS/Stack_Queue/climb_stairs.py
+++ DS/Stack_Queue/climb_stairs.py
@@ -1,11 +1,25 @@
-def f(n):
-    if n == 1:
-        return 1
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        hash_table = {}
 
-    if n == 2:
-        return 2
+        def f(n):
+            if n == 1:
+                return 1
 
-    return f(n-1) + f(n-2)
+            if n == 2:
+                return 2
+
+            value = 0
+            if n in hash_table:
+                return hash_table[n]
+            else:
+                value = f(n-1) + f(n-2)
+                hash_table[n] = value
+
+            return value
+
+        return f(n)
 
 
-print(f(4))
+solution = Solution()
+print(solution.climbStairs(45))
