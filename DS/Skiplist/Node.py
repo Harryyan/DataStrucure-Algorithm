561e58c39ea09b067af2ccb3d3c9bc0d2d33f3d4
harryyan1238 <harryyan1238@gmail.com>
2021-04-18 09:32:28 +1200

    finish insert

--- DS/Skiplist/Node.py
+++ DS/Skiplist/Node.py
@@ -1,14 +1,7 @@
 from typing import List
 
-
 class Node:
-
-    
-    def __init__(self, value, maxlevel):
-        self.value = value
-            # 当前索引总层数。
-            # 索引从 0 开始计数，到 maxLevel-1 为止。
-            # 第 0 层为原始链表，从下往上依次建立索引，最上层为第 maxLevel-1 层索引。
-
-        self.maxLevel = 0
-        self.forwards = [None] * maxlevel
+    def __init__(self, value, level):
+        self._value = value
+        # 当前节点在每个索引层的下一个节点
+        self._forwards = [None] * level
--- DS/Skiplist/skip_list.py
+++ DS/Skiplist/skip_list.py
@@ -1,14 +1,44 @@
 from Node import Node
 import random
 
+
 class SkipList:
-    MAX_LEVEL = 16
+    _MAX_LEVEL = 8
 
     def __init__(self):
+        # 当前索引总层数
         self.levelCount = 1
 
         # 带头结点
-        head = Node()
+        self.head = Node()
+        self.head._forwards = [None] * self._MAX_LEVEL
 
-    def random_level(self):
-        return 0
+    def insert(self, value):
+        level = self._random_level()
+        if self._level_count < level:
+            self._level_count = level
+
+        new_node = Node(value, [None] * level)
+        update = [self.head] * level
+
+        p = self.head
+        for i in range(level - 1, -1, -1):
+            while p._forwards[i] and p._forwards[i]._data < value:
+                p = p._forwards[i]
+            if p._forwards[i] and p._forwards[i]._data == value:
+                # 说明已经存储该节点，不需要再插入
+                return False
+            update[i] = p  # 找到插入的位置
+
+        for i in range(level):
+            new_node._forwards[i] = update[i]._forwards[i]  # new_node.next = prev.next
+            update[i]._forwards[i] = new_node  # prev.next = new_node
+
+        return True
+
+    # 确保Node添加到每层索引的概率是1 / 2
+    def _random_level(self, p=0.5):
+        level = 1
+        while random.random() < p and level < self._MAX_LEVEL:
+            level += 1
+        return level
