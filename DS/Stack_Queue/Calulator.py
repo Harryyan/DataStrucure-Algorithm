1f8795e5fb76a1fde91a13437b8e517b0b1cc0bc
harryyan1238 <harryyan1238@gmail.com>
2021-04-28 23:11:20 +1200

    bug free version

--- DS/Stack_Queue/Calulator.py
+++ DS/Stack_Queue/Calulator.py
@@ -1,107 +1,28 @@
-from os import truncate
+class Solution(object):
+    def calculate(self, s):
+        res, num, sign = 0, 0, 1
+        stack = []
+        for c in s:
+            if c.isdigit():
+                num = 10 * num + int(c)
+            elif c == "+" or c == "-":
+                res += sign * num
+                num = 0
+                sign = 1 if c == "+" else -1
+            elif c == "(":
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ")":
+                res += sign * num
+                num = 0
+                res *= stack.pop()
+                res += stack.pop()
+        res += sign * num
+        return res
 
 
-class Solution:
-    def _cal(self, numbers, operators) -> int:
-        if not numbers:
-            return 0
-
-        if not operators:
-            if numbers:
-                n = 0
-                i = 0
-                while numbers:
-                    n += numbers.pop() * 10 ** i
-                    i += 1
-                return n
-            else:
-                return 0
-
-        test = False
-
-        if len(operators) > len(numbers):
-            test = True
-        else:
-            test = False
-
-        o = operators.pop()
-
-        n1 = 0
-        i = 0
-        n2 = 0
-
-        if numbers:
-            n2 = numbers.pop()
-
-            if test:
-                n1 = 0
-            else:
-                while numbers:
-                    n1 += numbers.pop() * 10 ** i
-
-                    if operators:
-                        break
-
-                    i += 1
-
-        if o == "+":
-            n = int(n2) + int(n1)
-        elif o == "-":
-            n = int(n1) - int(n2)
-        else:
-            n = n2
-            numbers.append(n)
-
-            n = self._cal(numbers, operators)
-
-        return n
-
-    def calculate(self, s: str) -> int:
-        if not s:
-            return 0
-        numbers = []
-        operators = []
-
-        for s1 in s:
-            if s1.isdigit():
-                numbers.append(int(s1))
-            elif s1 != " ":
-                if operators:
-                    if s1 == "(":
-                        operators.append(s1)
-                        continue
-
-                    if (s1 == "+" or s1 == "-") and operators[-1] == "(":
-                        operators.append(s1)
-                        continue
-
-                    if s1 == ")":
-                        n = self._cal(numbers, operators)
-
-                        numbers.append(n)
-
-                        if operators:
-                            operators.pop()
-                        continue
-
-                    n = self._cal(numbers, operators)
-
-                    numbers.append(n)
-                    operators.append(s1)
-                    print(numbers, end=",")
-                    print(operators)
-                    print("\n")
-                else:
-                    operators.append(s1)
-
-        n = self._cal(numbers, operators)
-
-        numbers.append(n)
-
-        return numbers.pop()
-
-
-test = "12-(+3-1)"
-obj = Solution()
-
-print(obj.calculate(test))
+test = Solution()
+str = "123-(-13+2) + 13 - 2 + (9 + 4)"
+print(test.calculate(str))
