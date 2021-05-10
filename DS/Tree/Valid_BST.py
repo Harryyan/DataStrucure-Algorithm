0062b65d83b8eda83d9589107039ec31e76334a0
harryyan1238 <harryyan1238@gmail.com>
2021-05-10 19:50:23 +1200

    leetcode_104

--- DS/Tree/Valid_BST.py
+++ DS/Tree/Valid_BST.py
@@ -27,3 +27,38 @@ class Solution:
             self._values.append(node.val)
 
         right = self.depth_iteration_middle(node.right)
+
+
+class Solution2:
+    def maxDepth(self, root: TreeNode) -> int:
+        x = 1
+
+        def find_max(node, x) -> int:
+            if not node:
+                return x
+
+            x += 1
+
+            l_m = find_max(node.left, x)
+
+            x -= 1
+
+            l_r = find_max(node.right, x)
+
+            x -= 1
+
+            return max(l_m, l_r)
+
+        return find_max(root, x)
+
+
+node_9 = TreeNode(9, None, None)
+node_15 = TreeNode(15, None, None)
+node_7 = TreeNode(7, None, None)
+node_20 = TreeNode(20, node_15, node_7)
+node_3 = TreeNode(3, node_9, node_20)
+
+test = Solution2()
+result = test.maxDepth(node_3)
+
+print(result)
