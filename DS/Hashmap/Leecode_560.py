9165188fe8a0b09efea7f97c8712bb253d19a794
Harry Yan <harryyan1238@gmail.com>
2021-05-19 22:38:08 +1200

    add questions

--- DS/Hashmap/Leecode_560.py
+++ DS/Hashmap/Leecode_560.py
@@ -1,6 +1,8 @@
 import collections
 from typing import List
 
+# 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+
 class Solution:
     def subarraySum(self, nums: List[int], target: int) -> int:
         pre_sum = collections.defaultdict(int)
