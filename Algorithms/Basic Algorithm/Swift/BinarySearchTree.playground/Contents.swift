482c14201a9e4b74f7887f404a9a32eec6a48bf9
harryyan1238 <harryyan1238@gmail.com>
2021-05-25 07:59:10 +1200

    change strcutures

--- Algorithms/Basic Algorithm/Swift/BinarySearchTree.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-enum BinarySearchTree<T: Comparable> {
-    case empty
-    case leaf(value: T)
-    indirect case node(value: T, left: BinaryTree<T>, right: BinaryTree<T>, parent: BinaryTree<T>?)
-}
