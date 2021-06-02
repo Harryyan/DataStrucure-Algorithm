13c0b6fcc63cc771b6337212a6d4d0cb09f3710d
Harry Yan <harryyan1238@gmail.com>
2021-06-02 19:13:02 +1200

    finish lcs

--- practices/DP/LongestCommonSubSeq.py
+++ practices/DP/LongestCommonSubSeq.py
@@ -9,12 +9,33 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n = len(text1)
         m = len(text2)
+        if n == 0 or m == 0:
+            return 0
         
-        if n == m == 1 and text1 == text2:
-            return 1
+        dp = [0 for _ in range(m + 1)]
         
+        for i in range(1, n + 1):
+            left_up = 0 # 对角线
+            dp[0] = 0
+            
+            for j in range(1, m + 1):
+                left = dp[j-1]
+                up = dp[j]
+                
+                if text1[i-1] == text2[j-1]:
+                    dp[j] = left_up + 1
+                else:
+                    dp[j] = max([left, up])
+                
+                left_up = up 
         
-        dp = [[ 0 for _ in range(0,n) ] for _ in range(0,m)]
-        
-        
-        return 0
+        return dp[-1]
+    
+    
+    
+s = Solution()
+a = "aaa"
+b = "new zelaand"
+
+result = s.longestCommonSubsequence(a,b)
+print(result)
