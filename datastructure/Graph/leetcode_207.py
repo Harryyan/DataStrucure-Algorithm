3d6769045d58a08677247bd7121bb4f4dfbfa796
harryyan1238 <harryyan1238@gmail.com>
2021-06-20 21:42:34 +1200

    finish

--- datastructure/Graph/leetcode_207.py
+++ datastructure/Graph/leetcode_207.py
@@ -1,15 +1,51 @@
 # 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
 
-# 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+# 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，
+# 表示如果要学习课程 ai 则 必须 先学习课程  bi 。
 
 # 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
 # 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
 
-from typing import List
+from typing import DefaultDict, List
+
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        pass
-    
-    
-    
+        finalNumbers = 0
+
+        # 我依赖谁
+        inDeg = DefaultDict(list)
+        # 谁依赖我
+        outDeg = DefaultDict(list)
+
+        for item in prerequisites:
+            inDeg[item[0]].append(item[1])
+            outDeg[item[1]].append(item[0])
+
+        queue = []
+        for item in range(0, numCourses):
+            if len(inDeg[item]) == 0:
+                queue.append(item)
+
+        if len(queue) == 0:
+            return False
+
+        print(queue)
+        while queue:
+            item = queue.pop(0)
+            outs = outDeg[item]
+
+            for out in outs:
+                if len(inDeg[out]) == 1:
+                    queue.append(out)
+                    finalNumbers += 1
+                elif len(inDeg[out]) > 1:
+                    inDeg[out].remove(item)
+
+        return finalNumbers == numCourses - 1
+
+
+so = Solution()
+
+result = so.canFinish(3, [[0, 1], [2, 1]])
+print(result)
