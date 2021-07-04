c551be63265cc01625b82bb943d1751113c95d3e
Harry Yan <harryyan1238@gmail.com>
2021-07-04 20:48:33 +1200

    leetcode_198

---
+++ practices/DP/leetcode_198.py
@@ -0,0 +1,23 @@
+
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1: return nums[0]
+        if len(nums) == 2: return max(nums[0], nums[1])
+        
+        dp = [0,nums[0]]
+        
+        for i in range(1, len(nums)):
+            temp = dp[0]
+            dp[0] = max(temp,dp[1])
+            dp[1] = temp+nums[i]
+        
+        return max(dp[0], dp[1])
+    
+    
+s = Solution()
+sample = [2,1,1,2]
+r = s.rob(sample)
+
+print(r)
