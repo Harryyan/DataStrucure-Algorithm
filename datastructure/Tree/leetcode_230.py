acf605f7cdb48a5ec89b2f22ad03e513d3826abd
Harry Yan <harryyan1238@gmail.com>
2021-07-08 23:05:37 +1200

    add leetcode_230

---
+++ datastructure/Tree/leetcode_230.py
@@ -0,0 +1,8 @@
+from Node import TreeNode
+
+# 给定一个二叉搜索树的根节点 root ，
+# 和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        return 0
