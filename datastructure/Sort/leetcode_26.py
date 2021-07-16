8cc6077a3c64d9b4f3fd683d50665542bacd32c5
Harry Yan <harryyan1238@gmail.com>
2021-07-17 11:06:55 +1200

    add 2 fast and slow pointers methods

--- datastructure/Sort/leetcode_26.py
+++ datastructure/Sort/leetcode_26.py
@@ -1,6 +1,6 @@
 from typing import List
 
-class Solution:
+class Solution1:
     def removeDuplicates(self, nums: List[int]) -> int:
         if not nums: return 0
         if len(nums) == 1: return 1
@@ -17,7 +17,20 @@ class Solution:
             i += 1 
             
         return len(nums)
-        
+
+
+# 双指针 -- 快，慢指针
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+
+
+      
 nums = [0,0,1,1,1,2,2,3,3,4]
 s = Solution()
 
