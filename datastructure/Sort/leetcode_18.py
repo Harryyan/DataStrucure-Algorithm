1f5c6f5c6d8976efe1a58a91e4ba01b68c344a21
Harry Yan <harryyan1238@gmail.com>
2021-07-17 10:05:54 +1200

    add sum of N

--- datastructure/Sort/leetcode_18.py
+++ datastructure/Sort/leetcode_18.py
@@ -46,9 +46,96 @@ class Solution:
                     else:
                         L=L+1
         return res
+
+# 排序 +双指针
+class Soluction_Advanced:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+         ans = []
+         if len(nums) < 4:
+            return ans;
+        
+         nums.sort()
+         length = len(nums)
+         
+         for i in range(length):
+             if nums[i] > 0 and target < 0:
+                    break
+                
+             if i > 0 and nums[i] == nums[i-1]:
+                continue
+                
+             for j in range(i+1, length):
+                 if j > i+1 and nums[j] == nums[j-1]:
+                        continue
+                 two_sum = nums[i] + nums[j]
+                
+                 if two_sum > 0 and target < 0:
+                    break
+
+                 left, right = j + 1, length - 1
+        
+                 while left < right:
+                    four_sum = two_sum + nums[left] + nums[right]
+                    # residual = target - nums[i] - nums[j] - nums[left] - nums[right]
+                    if four_sum == target:
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left+1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right-1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+                    elif four_sum < target:
+                        left += 1
+                    else:
+                        right -= 1
+        
+             return ans
+
+# 多数之和 - 使用递归
+class Solution_NSum:
+        def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+            def nSum(nums: List[int], n: int, target: int) -> List[List[int]]:
+                res = []
+                if len(nums) < n:
+                    return res
+                if n == 2:
+                    left, right = 0, len(nums) - 1
+                    while left < right:
+                        if nums[left] + nums[right] == target:
+                            res.append([nums[left], nums[right]])
+                            while left < right and nums[left] == nums[left+1]:
+                                left += 1
+                            while left < right and nums[right] == nums[right-1]:
+                                right -= 1
+                            left += 1
+                            right -= 1
+                        elif nums[left] + nums[right] < target:
+                            left += 1
+                        else:
+                            right -= 1
+                    return res
+                else:
+                    for i in range(len(nums)-n+1):
+                        if i > 0 and nums[i] == nums[i-1]:
+                            continue
+                        min_sum = sum(nums[i:i+n])
+                        if min_sum > target:
+                            break
+                        max_sum = nums[i] + sum(nums[-n+1:])
+                        if max_sum < target:
+                            continue
+                        sub_res = nSum(nums[i+1:], n-1, target-nums[i])
+                        for j in range(len(sub_res)):
+                            res.append([nums[i]]+sub_res[j])
+                    return res
+            nums.sort()
+            res = nSum(nums, 4, target)
+            return res 
+    
     
 nums = [1,-2,-5,-4,-3,3,3,5]
 target = -11
-s = Solution()
+s = Solution_NSum()
 
 print(s.fourSum(nums, target))
