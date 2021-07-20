ec8c20a71fbfb658281fd8c31253245a7ff8403a
Harry Yan <harryyan1238@gmail.com>
2021-07-20 16:00:14 +1200

    leetcode_53

---
+++ 分类/动归/题目/leetcode_53-最大连续子序和.py
@@ -0,0 +1,17 @@
+
+from typing import List
+
+# 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+
+ 
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0 for _ in range(n)]
+        dp[0] = nums[0]
+        for i in range(1, n):
+            dp[i] = max(dp[i-1] + nums[i], nums[i])
+        
+        res = max(dp)
+        return res
