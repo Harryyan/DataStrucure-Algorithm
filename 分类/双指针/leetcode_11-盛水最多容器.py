2ccbca1482afae76686f37858f7b18cd07860a1f
Harry Yan <harryyan1238@gmail.com>
2021-07-24 19:05:42 +1200

    double pointers

---
+++ 分类/双指针/leetcode_11-盛水最多容器.py
@@ -0,0 +1,37 @@
+# Q3:
+# 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点(i,ai).
+# 在坐标内画 n 条垂直线，垂直线 i的两个端点分别为(i,ai) 和 (i, 0) .
+# 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。说明：你不能倾斜容器
+from typing import List
+
+class Solution:
+    def maxArea(self, items: List[int]) -> int:
+        start = 0
+        end = len(items) - 1
+        area = 0
+
+        while start < end:
+            height = 0
+            interval = end - start
+
+            if items[start] > items[end]:
+                height = items[end]
+                end -= 1
+            else:
+                height = items[start]
+                start += 1
+
+            temp = interval * height
+
+            if temp > area:
+                area = temp
+
+        return area
+
+        
+
+items = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+s = Solution()
+result = s.maxArea(items)
+
+print(result)
