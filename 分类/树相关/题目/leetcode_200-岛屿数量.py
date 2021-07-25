d264d398d12f6351f4b3656349177a6e7483f6bd
Harry Yan <harryyan1238@gmail.com>
2021-07-25 21:45:54 +1200

    add leetcode_547

---
+++ 分类/树相关/题目/leetcode_200-岛屿数量.py
@@ -0,0 +1,46 @@
+from typing import List
+
+class Solution:
+    
+    def dfs(self, grid, i, j):
+        
+        if not ( 0 <= i < len(grid) and 0 <= j < len(grid[i])):
+            return
+        
+        if grid[i][j] != '1':
+            return 
+             
+        grid[i][j] = -1
+        
+        self.dfs(grid, i-1,j)
+        self.dfs(grid, i+1,j)
+        
+        self.dfs(grid, i,j-1)
+        self.dfs(grid, i,j+1)
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        
+        m = len(grid)
+        
+        for i in range(m):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        
+        return count
+    
+    
+nums =  [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+
+s = Solution()
+
+r = s.numIslands(nums)
+
+print(r)
---
+++ 分类/树相关/题目/leetcode_547-省份数量.py
@@ -0,0 +1,64 @@
+from typing import Deque, List
+
+# 有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
+
+# 省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
+
+# 给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
+
+# 返回矩阵中 省份 的数量。
+
+# DFS
+
+class Solution:
+    
+    def dfs(self, grid, i, j):    
+        grid[i][j] = -1
+        grid[j][i] = -1
+        
+        if i == j: return
+        
+        for x in range(len(grid)):
+            if grid[j][x] == 1:
+                self.dfs(grid, j, x)
+    
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+            n = len(isConnected)
+            c = 0
+            
+            for i in range(n):
+                for j in range(n):
+                    if isConnected[i][j] == 1:
+                        self.dfs(isConnected, i, j)
+                        c += 1
+            
+            return c
+ # BFS       
+class Solution2:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        count = 0
+        N = len(isConnected)
+        unvisited = [True] * N
+        de = Deque()
+        for i in range(N):
+            if unvisited[i]:
+                unvisited[i] = False
+                count += 1
+                de.append(i)
+                while de:
+                    city = de.popleft()
+                    for j in range(N):
+                        if isConnected[city][j] == 1 and unvisited[j]:
+                            unvisited[j] = False
+                            de.append(j)
+        return count
+
+
+        
+        
+isConnected = [[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]]
+s = Solution()
+
+r = s.findCircleNum(isConnected)
+
+print(r)
