4143e67d19b96cbb7d6885d9816115f31e1d00e3
Harry Yan <harryyan1238@gmail.com>
2021-07-25 21:50:21 +1200

    add dfs advanced version

--- 分类/树相关/题目/leetcode_547-省份数量.py
+++ 分类/树相关/题目/leetcode_547-省份数量.py
@@ -31,6 +31,29 @@ class Solution:
                         c += 1
             
             return c
+        
+        
+ # DFS - advanced
+
+class Solution2:
+        def findCircleNum(self, isConnected: List[List[int]]) -> int:
+            def dfs(i: int):
+                for j in range(provinces):
+                    if isConnected[i][j] == 1 and j not in visited:
+                        visited.add(j)
+                        dfs(j)
+        
+            provinces = len(isConnected)
+            visited = set()
+            circles = 0
+
+            for i in range(provinces):
+                if i not in visited:
+                    dfs(i)
+                    circles += 1
+        
+            return circles
+
  # BFS       
 class Solution2:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
