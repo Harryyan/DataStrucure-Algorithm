ef093c43efb4b353800656ce11b4962939ae43fb
Harry Yan <harryyan1238@gmail.com>
2021-08-09 19:15:18 +1200

    leetcode202

---
+++ 分类/双指针/leetcode_202-快乐数.py
@@ -0,0 +1,45 @@
+# 编写一个算法来判断一个数 n 是不是快乐数。
+
+# 「快乐数」定义为：
+
+# 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+# 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+# 如果 可以变为  1，那么这个数就是快乐数。
+# 如果 n 是快乐数就返回 true ；不是，则返回 false 。
+
+
+# 关键在于判断重复，类似链表，判断是否有环
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next(n):
+            total_sum = 0
+            while n > 0:
+                n, digit = divmod(n, 10)
+                total_sum += digit ** 2
+            return total_sum
+
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+
+        return n == 1
+
+# 双指针
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next(number):
+            total_sum = 0
+            while number > 0:
+                number, digit = divmod(number, 10)
+                total_sum += digit ** 2
+            return total_sum
+
+        slow_runner = n
+        fast_runner = get_next(n)
+        
+        while fast_runner != 1 and slow_runner != fast_runner:
+            slow_runner = get_next(slow_runner)
+            fast_runner = get_next(get_next(fast_runner))
+        return fast_runner == 1
