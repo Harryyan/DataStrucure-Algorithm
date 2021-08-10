c318b7b711bbfbe8a01d5194c3041dac3b11f019
harry yan <harryyan1238@gmail.com>
2021-08-11 08:53:56 +1200

    kth - 1st version

--- 分类/数组-字符串-链表/排序/题目/leetcode_215-第K个最大元素.py
+++ 分类/数组-字符串-链表/排序/题目/leetcode_215-第K个最大元素.py
@@ -2,4 +2,19 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return 0
+        nums.sort()
+
+        print(nums)
+        n = len(nums)
+        rest = n - k
+
+        return nums[rest]
+
+
+s = Solution()
+nums = [3,2,3,1,2,4,5,5,6]
+k = 4
+
+r = s.findKthLargest(nums, k)
+
+print(r)
