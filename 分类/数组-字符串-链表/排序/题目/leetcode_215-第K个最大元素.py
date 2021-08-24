37071d652658b40e8d988bfedd8081e4725ec2cd
Harry Yan <harryyan1238@gmail.com>
2021-08-25 06:14:13 +1200

    leetcode 215

--- 分类/数组-字符串-链表/排序/题目/leetcode_215-第K个最大元素.py
+++ 分类/数组-字符串-链表/排序/题目/leetcode_215-第K个最大元素.py
@@ -1,4 +1,6 @@
 from typing import List
+import heapq as hq
+
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
@@ -10,14 +12,59 @@ class Solution:
 
         return nums[rest]
     
-    
-    
-class Solution2:
+# 排序 ：时间复杂度 O(NlogN)，空间复杂度 O(1)  
+
+# 也可以用快排
+def quick_sort_search_kth(alist, first, last, k):
+    if k > len(alist):
+        return
+
+    if first > last:
+        return
+
+    mid_value = alist[first]
+    low = first
+    high = last
+
+    while low < high:
+        while low < high and alist[high] >= mid_value:
+            high -= 1
+        alist[low] = alist[high]
+
+        while low < high and alist[low] < mid_value:
+            low += 1
+        alist[high] = alist[low]
+
+    alist[low] = mid_value
+
+    if low + 1 == k:
+        print(alist[low])
+        return
+    elif low + 1 < k:
+        quick_sort_search_kth(alist, low + 1, last, k)
+    else:
+        quick_sort_search_kth(alist, first, low - 1, k)
+
+# 排序 ：时间复杂度 O(NlogN)，空间复杂度 O(1)  
+
+class Solution_Heap:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         # 使用堆
-        return 0
+        heap = []
+        
+        for num in nums:
+            hq.heappush(heap, num)
+            
+            if len(heap) > k:
+                hq.heappop(heap)
+        
+        return heap[0]
+    
+# 堆 ：时间复杂度 O(NlogK)，空间复杂度 O(K)。
+
 
 s = Solution()
+s1 = Solution_Heap()
 nums = [3,2,3,1,2,4,5,5,6]
 k = 4
 
