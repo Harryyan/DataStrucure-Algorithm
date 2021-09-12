a342961b92d87b968a24ab8306dc41ede19c26d3
Harry Yan <harryyan1238@gmail.com>
2021-09-12 19:19:24 +1200

    leetcode653

---
+++ 分类/树相关/题目/leetcode_653-两数之和BST.py
@@ -0,0 +1,26 @@
+from Node import TreeNode
+
+# 给定一个二叉搜索树 root 和一个目标结果 k，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true
+
+# 两数之和变体问题
+# 中序遍历之后
+# 双指针求两数之和\
+class Solution:
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        nums = []
+        def mid_order(root):
+            if root:
+                mid_order(root.left)
+                nums.append(root.val)
+                mid_order(root.right)
+        mid_order(root)
+        l, r = 0, len(nums) - 1
+        while l < r:
+            if nums[l] + nums[r] == k:
+                return True
+            elif nums[l] + nums[r] > k:
+                r -= 1
+            else:
+                l += 1
+                
+        return False
