32a344b8991e30ac73de855b40c79d1f5539b093
HarryYan <harryyan1238@gmail.com>
2021-10-16 17:49:29 +1300

    add ace solution

--- 分类/动归/leetcode_64-最小路径和.py
+++ 分类/动归/leetcode_64-最小路径和.py
@@ -4,6 +4,8 @@ from typing import List
 # 说明：每次只能向下或者向右移动一步。
 
 class Solution:
+    # 时间复杂度: O(mn)
+    # 空间复杂度: O(mn)
     def minPathSum(self, grid: List[List[int]]) -> int:
         if not grid: return 0
 
@@ -13,23 +15,51 @@ class Solution:
         n = len(grid[0])  
 
         min_dist = [[0] * n for _ in range(m)]
-        
         min_dist[0][0] = grid[0][0]
 
+        # 初始化第0行数据
         for j in range(1, n):
             min_dist[0][j] = grid[0][j] + min_dist[0][j-1]
 
+        # 初始化第0列数据
         for i in range(1, m):
             min_dist[i][0] = grid[i][0] + min_dist[i-1][0]
 
+        # 从(1,1)开始逐步计算局部最优解
         for i in range(1, m):
             for j in range(1, n):
                 min_dist[i][j] = grid[i][j] + min(min_dist[i][j-1], min_dist[i-1][j])
         
         return min_dist[-1][-1]
 
-grid = [[1,3,1],[1,5,1],[4,2,1]]
-s = Solution()
+class Solution_Ace:
+    # 时间复杂度: O(n)
+    # 空间复杂度: O(mn)
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        if not grid: return 0
+
+        # 行
+        m = len(grid)
+        # 列
+        n = len(grid[0]) 
+
+        min_dis = [0] * n
+        min_dis[0] = grid[0][0]
+
+        for j in range(1,n):
+            min_dis[j] = min_dis[j-1] + grid[0][j]
+
+        for i in range(1,m):
+            min_dis[0] += grid[i][0]
+
+            for j in range(1,n):
+                min_dis[j] = min(min_dis[j-1], min_dis[j]) + grid[i][j]
+               
+        return min_dis[-1]
+
+
+grid = [[1]]
+s = Solution_Ace()
 
 r = s.minPathSum(grid)
 print(r)
