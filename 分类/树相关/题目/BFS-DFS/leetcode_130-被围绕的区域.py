0d140631c36fa9216a40df6a42f90f04a50c8e1f
HarryYan <harryyan1238@gmail.com>
2021-10-18 23:03:31 +1300

    update

--- 分类/树相关/题目/BFS-DFS/leetcode_130-被围绕的区域.py
+++ 分类/树相关/题目/BFS-DFS/leetcode_130-被围绕的区域.py
@@ -1,7 +1,9 @@
 # 给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
-
+# leetcode-130
 
 class Solution:
+    # 时间复杂度: O(mn)
+    # 空间复杂度: O(1)
     def solve(self, board):
         row, col = len(board), len(board[0])
 
