1bc31f777a5ce62b63e764056cf2b1ad14a1efc8
HarryYan <harryyan1238@gmail.com>
2021-10-22 19:21:12 +1300

    comments

--- 分类/动归/题目/leetcode_95-不同BSTII.py
+++ 分类/动归/题目/leetcode_95-不同BSTII.py
@@ -1,7 +1,7 @@
 from typing import List
 
 # 给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。可以按 任意顺序 返回答案。
-# leetcode 95
+# leetcode-95
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
