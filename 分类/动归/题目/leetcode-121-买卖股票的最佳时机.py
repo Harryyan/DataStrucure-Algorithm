66692c36f42096b65a5251ab422b9090129644f0
HarryYan <harryyan1238@gmail.com>
2021-10-25 16:40:52 +1300

    leetcode 121

--- 分类/动归/题目/leetcode-121-买卖股票的最佳时机.py
+++ 分类/动归/题目/leetcode-121-买卖股票的最佳时机.py
@@ -4,8 +4,29 @@ from typing import List
 # 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
 # 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0.
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+# leetcode - 121
 
-        
-        return 0
+class Solution:
+
+    # 时间复杂度: O(n)
+    # 空间复杂度: O(1)
+
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices: return None
+
+        maxProfit = 0
+        minPrice = float('inf')
+
+        for price in prices:
+            maxProfit = max(maxProfit, price - minPrice)
+            minPrice = min(minPrice, price)
+
+        return maxProfit
+
+prices = [7,1,5,3,6,4]
+
+s = Solution()
+
+r = s.maxProfit(prices)
+
+print(r)
--- 分类/动归/题目/leetcode_122-买卖股票的最佳时机II.py
+++ 分类/动归/题目/leetcode_122-买卖股票的最佳时机II.py
@@ -1,5 +1,8 @@
-
 from typing import DefaultDict, List
+
+# 给定一个数组 prices ，其中 prices[i] 是一支给定股票第 i 天的价格。
+# 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+# 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
 
 class Solution:
      def maxProfit(self, prices: List[int]) -> int:
@@ -23,7 +26,7 @@ class Solution:
         return dp[0]
     
 s = Solution()
-prices =  [7,6,4,3,1]
+prices =  [7,1,5,3,6,4]
 
 result = s.maxProfit(prices)
 print(result)
