16219c836574995fd4b41ba89f210cfa0ad01cae
HarryYan <harryyan1238@gmail.com>
2021-10-27 07:41:53 +1300

    add comments

--- 分类/动归/题目/leetcode_152-乘积最大子数组.py
+++ 分类/动归/题目/leetcode_152-乘积最大子数组.py
@@ -18,7 +18,7 @@ class Solution:
         dp_max[0] = nums[0]
         dp_min[0] = nums[0]
 
-        
+        # 巧妙解决了附属符号问题，不需要记录sign； 无后效性；
         for i in range(1, n):
             dp_max[i] = max(nums[i], dp_max[i - 1] * nums[i], dp_min[i - 1] * nums[i])  
             dp_min[i] = min(nums[i], dp_max[i - 1] * nums[i], dp_min[i - 1] * nums[i])            
@@ -31,7 +31,7 @@ class Solution:
 # 当负数个数为奇数时候，它的左右两边的负数个数一定为偶数，只需求两边最大值
 # 当有 0 情况，重置就可以了
 
-class Solution_2:
+class Solution_Cool:
     # 时间复杂度：O(n)
     # 空间复杂度：O(n)
     def maxProduct(self, nums: List[int]) -> int:
@@ -39,11 +39,13 @@ class Solution_2:
 
         for i in range(1, len(nums)):
             nums[i] *= nums[i - 1] or 1
+            # 防止 0 的出现，出现了就从当前元素重新开始
             reverse_nums[i] *= reverse_nums[i - 1] or 1
+            print(reverse_nums[i])
 
         return max(nums + reverse_nums)
 
-nums = [3,-2,-2,4,3]
+nums = [3,-2,-2,0,3]
 s = Solution_2()
 
 r = s.maxProduct(nums)
