28b418cedf0e7170903be65449b069c7c2e9860d
HarryYan <harryyan1238@gmail.com>
2021-10-30 20:21:58 +1300

    leetcode - 233

---
+++ 分类/动归/题目/leetcode_233-1的个数.py
@@ -0,0 +1,18 @@
+# 给定一个整数 n，计算所有小于等于 n 的非负整数中数字 1 出现的个数。
+# LeetCode- 233
+
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        # edge
+        if n == 0: return 0
+        if n < 10: return 1
+
+        total = 0
+
+
+        
+
+
+
+
+        return total
