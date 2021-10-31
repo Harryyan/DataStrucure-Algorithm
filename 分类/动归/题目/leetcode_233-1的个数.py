6390b668c45154e6cebdae48415793c3458b5912
HarryYan <harryyan1238@gmail.com>
2021-10-31 15:36:40 +1300

    update code

--- 分类/动归/题目/leetcode_233-1的个数.py
+++ 分类/动归/题目/leetcode_233-1的个数.py
@@ -42,15 +42,14 @@ class Solution:
         high, cur, low = n // 10, n % 10, 0
 
         while high != 0 or cur != 0: # 当 high 和 cur 同时为 0 时，说明已经越过最高位，因此跳出
+            if cur == 0: res += high * digit
+            elif cur == 1: res += high * digit + low + 1
+            else: res += (high + 1) * digit
+
             low += cur * digit # 将 cur 加入 low ，组成下轮 low
             cur = high % 10 # 下轮 cur 是本轮 high 的最低位
             high //= 10 # 将本轮 high 最低位删除，得到下轮 high
             digit *= 10 # 位因子每轮 × 10
-
-            low += cur * digit
-            cur = high % 10
-            high //= 10
-            digit *= 10
 
         return res
 
