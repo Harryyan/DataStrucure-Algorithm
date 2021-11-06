71d6db7175acaa7f0c3f39091565b4eb92566b40
HarryYan <harryyan1238@gmail.com>
2021-11-07 08:57:53 +1300

    leetcode 231

---
+++ 分类/位操作/leetcode_231-2的幂.py
@@ -0,0 +1,16 @@
+# 给你一个整数 n，请你判断该整数是否是 2 的幂次方。如果是，返回 true ；否则，返回 false 。
+# 如果存在一个整数 x 使得 n == 2x ，则认为 n 是 2 的幂次方。
+
+# leetcode - 231
+
+# lowbit 运算
+# lowbit(n) = n & -n
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and (n & -n) == n
+
+
+s = Solution()
+
+r = s.isPowerOfTwo(32)
+print(r)
