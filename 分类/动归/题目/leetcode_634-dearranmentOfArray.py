140ae66d4368b1ac1a75f1ec3419ff188f4f9d1d
HarryYan <harryyan1238@gmail.com>
2021-11-14 09:25:52 +1300

    optimize sc of 634

---
+++ 分类/动归/题目/leetcode_634-dearranmentOfArray.py
@@ -0,0 +1,41 @@
+# In combinatorial mathematics, a derangement is a permutation of the elements of a set, such that no element appears in its original position.
+# You are given an integer n. There is originally an array consisting of n integers from 1 to n in ascending order, 
+# return the number of derangements it can generate. Since the answer may be huge, return it modulo 109 + 7.
+
+# leetcode 634
+
+class Solution:
+    # tc: O(n)
+    # sc: O(n)
+    def findDerangement(self, n: int) -> int:
+        if n < 2: return 0
+
+        dp = [0] * (n+1)
+
+        dp[0] = 0
+        dp[1] = 0
+        dp[2] = 1
+
+        for i in range(3, n+1):
+            dp[i] = (i - 1) * (dp[i-1] + dp[i-2])
+            dp[i] = dp[i] % (10**9 + 7)
+
+        return dp[-1]
+
+class Solution_Opt:
+    # tc: O(n)
+    # sc: O(1)
+    def findDerangement(self, n: int) -> int:
+        if n == 1: return 0
+        if n == 2: return 1
+        mod, a, b = 10 ** 9 + 7, 1, 0
+        for i in range(3, n+1):
+            # update f(n-1) and f(n-2)
+            a, b = (i-1) * (a + b) % mod, a
+        return a
+
+
+s = Solution_Opt()
+r = s.findDerangement(3)
+
+print(r)
