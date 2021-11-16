968ec0d5922844ba8c1e1fe72d08af9ba442cdc8
HarryYan <harryyan1238@gmail.com>
2021-11-16 18:55:38 +1300

    add comments

--- 分类/动归/题目/leetcode_279.py
+++ 分类/动归/题目/leetcode_279.py
@@ -1,5 +1,3 @@
-import math
-
 # Given an integer n, return the least number of perfect square numbers that sum to n.
 # A perfect square is an integer that is the square of an integer; in other words, 
 # it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
@@ -7,7 +5,7 @@ import math
 # leetcode - 279
 
 class Solution:
-    # 超时 O(n^2)
+    # overtime O(n^2)
     def numSquares(self, n: int) -> int:
         INF = 10 ** 5
         dp = [INF] * n
@@ -31,6 +29,7 @@ class Solution_Opt:
     def numSquares(self, n: int) -> int:
         dp=[i for i in range(n+1)]
         for i in range(2,n+1):
+            # reduce iterations
             for j in range(1,int(i**(0.5))+1):
                 dp[i]=min(dp[i],dp[i-j*j]+1)
         return dp[-1]
