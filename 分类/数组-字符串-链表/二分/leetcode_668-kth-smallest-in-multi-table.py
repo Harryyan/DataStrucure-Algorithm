ce0b6c42f7d4f84dce80a6f7dab5026a837cb9e5
HarryYan <harryyan1238@gmail.com>
2021-11-17 17:38:05 +1300

    remove heap

--- 分类/数组-字符串-链表/二分/leetcode_668-kth-smallest-in-multi-table.py
+++ 分类/数组-字符串-链表/二分/leetcode_668-kth-smallest-in-multi-table.py
@@ -1,5 +1,3 @@
-import heapq as hq
-
 # Nearly everyone has used the Multiplication Table. The multiplication table of size m x n 
 # is an integer matrix mat where mat[i][j] == i * j (1-indexed).
 # Given three integers m, n, and k, return the kth smallest element in the m x n multiplication table.
