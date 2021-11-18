da2e4f971c441542a542bb9da9d244f30f1a0459
HarryYan <harryyan1238@gmail.com>
2021-11-18 18:15:57 +1300

    646

--- 分类/动归/题目/leetcode_646-longest-pair-chain.py
+++ 分类/动归/题目/leetcode_646-longest-pair-chain.py
@@ -1,4 +1,5 @@
 from typing import List
+import bisect
 
 # You are given an array of n pairs pairs where pairs[i] = [lefti, righti] and lefti < righti.
 # A pair p2 = [c, d] follows a pair p1 = [a, b] if b < c. A chain of pairs can be formed in this fashion.
@@ -6,9 +7,8 @@ from typing import List
 # You do not need to use up all the given intervals. You can select pairs in any order.
 
 class Solution:
-    def isOverlapped(self, interval: List[int], newInterval: List[int]) -> bool:
-        return not (interval[1] < newInterval[0] or interval[0] > newInterval[1])
-
+    # tc: O(n^2)
+    # sc: O(n)
     def findLongestChain(self, pairs: List[List[int]]) -> int:
         n = len(pairs)
         pairs.sort(key=lambda x: x[0])
@@ -17,15 +17,54 @@ class Solution:
         
         for i in range(1, n):
             for j in range(0, i):
-                if not self.isOverlapped(pairs[i], pairs[j]):
+                if pairs[i][0] > pairs[j][1]:
                     dp[i] = max(dp[i], dp[j] + 1)
                 else:
                     dp[i] = max(dp[i], dp[j])
         return dp[-1]
 
+class Solution_Bisect:
+    # tc: O(nlogn)
+    # sc: O(n)
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pairs.sort()
+        n = len(pairs)
+        tail = [pairs[0][1]]
 
-pairs = [[1,2],[7,8],[4,5]]
-s =Solution()
+        for i in range(1, n):
+            # 二分
+            idx = bisect.bisect_left(tail, pairs[i][0])
+
+            if idx == len(tail):
+                tail.append(pairs[i][1])
+
+            if pairs[i][1] < tail[idx]:
+                # 这里其实也是贪心
+                tail[idx] = pairs[i][1]
+
+        return len(tail)
+
+class Solution_Greedy:
+    # tc: O(nlogn) - sorting
+    # sc: O(n)
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        if not pairs:
+            return 0
+
+        pairs.sort(key=lambda x: x[1])
+        count = 1
+        cur = pairs[0][1]
+
+        for i in range(1, len(pairs)):
+            if pairs[i][0] > cur:
+                count += 1
+                cur = pairs[i][1]
+
+        return count
+
+
+pairs = [[1,8],[6,10],[9,12], [13,15]]
+s =Solution_Greedy()
 
 r = s.findLongestChain(pairs)
 print(r)
