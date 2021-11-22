581c8833e5c900fb1089ee143b4272dfdea70bdb
HarryYan <harryyan1238@gmail.com>
2021-11-22 21:18:53 +1300

    update

--- 分类/贪心/题目/leetcode_277-Find-Celebrity.py
+++ 分类/贪心/题目/leetcode_277-Find-Celebrity.py
@@ -1,9 +1,12 @@
 
-# Suppose you are at a party with n people (labeled from 0 to n - 1), and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know him/her, but he/she does not know any of them.
-
-# Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information about whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
-
-# You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n). There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
+# Suppose you are at a party with n people (labeled from 0 to n - 1), and among them, there may exist one celebrity. 
+# The definition of a celebrity is that all the other n - 1 people know him/her, but he/she does not know any of them.
+# Now you want to find out who the celebrity is or verify that there is not one. 
+# The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" 
+# to get information about whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+# You are given a helper function bool knows(a, b) which tells you whether A knows B. 
+# Implement a function int findCelebrity(n). There will be exactly one celebrity 
+# if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
 
 # The knows API is already defined for you.
 # return a bool, whether a knows b
@@ -12,6 +15,8 @@ def knows(a: int, b: int) -> bool:
     return True
 
 class Solution(object):
+    # tc: O(n)
+    # sc: O(1)
     def findCelebrity(self, n):
         """
         :type n: int
@@ -20,18 +25,14 @@ class Solution(object):
         candidate = 0
 
         for j in range(1, n):
-            # 假如 candidate认识j
             if knows(candidate, j): 
                 candidate = j
 
         for j in range(n):
-            # 名人只认识自己，跳过
             if candidate == j:
                 continue
-            # 假如发现名人还认识别人，说明它不是名人
             if knows(candidate, j):
                 return -1
-            # 假如别人不认识名人，说明它不是名人
             if not knows(j, candidate):
                 return -1
         
