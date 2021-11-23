456639fd3a24822672c45367f968929baf9ccb2f
HarryYan <harryyan1238@gmail.com>
2021-11-23 22:14:59 +1300

    leetcode 274

--- 分类/数组-字符串-链表/二分/leetcode_274-H-Index.py
+++ 分类/数组-字符串-链表/二分/leetcode_274-H-Index.py
@@ -1,20 +1,19 @@
 from typing import List
 
 # Given an array of integers citations where citations[i] is the number of citations a researcher received for their 
-# ith paper and citations is sorted in an ascending order, return compute the researcher's h-index.
-# According to the definition of h-index on Wikipedia: A scientist has an index h if 
-# h of their n papers have at least h citations each, and the other n − h papers have no more than h citations each.
+# ith paper, return compute the researcher's h-index.
+# According to the definition of h-index on Wikipedia: A scientist has an
+# index h if h of their n papers have at least h citations each, and the other n − h papers have no more than h citations each.
 # If there are several possible values for h, the maximum one is taken as the h-index.
-# You must write an algorithm that runs in logarithmic time.
 
 
 class Solution:
-    # tc: O(logn)
-    # sc: O(1)
     def hIndex(self, citations: List[int]) -> int:
         low = 0
         n = len(citations)
         high = n - 1
+
+        citations.sort()
 
         while low <= high:
             mid = low + ((high - low) >> 1)
@@ -30,7 +29,7 @@ class Solution:
 
         return n - low
 
-citations = [0,1,3,5,6]
+citations = [1,3,1]
 s = Solution()
 
 r = s.hIndex(citations)
