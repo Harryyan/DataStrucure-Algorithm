7fe7889db13ddb8f1d2008dd123a6903df662570
HarryYan <harryyan1238@gmail.com>
2021-11-25 21:08:36 +1300

    leetcode 334

---
+++ 分类/动归/题目/leetcode_334-Triplet-Subsequence.py
@@ -0,0 +1,36 @@
+from typing import List
+
+# Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k 
+# and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+# leetcode - 334
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n < 3: return False
+
+        dp = [1] * n
+        count = [1] * n
+        
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:  
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        count[i] = count[j]
+                        
+                    elif dp[j] + 1 == dp[i]:
+                        count[i] += count[j]
+                        
+            if dp[i] == 3:
+                return True
+
+        return False
+
+nums = [2,1,5,0,4,6]
+s = Solution()
+
+r = s.increasingTriplet(nums)
+
+print(r)
