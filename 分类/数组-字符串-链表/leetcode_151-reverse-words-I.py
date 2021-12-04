a3065516063f5dc4a8b445bbabc951fc41cb36f9
HarryYan <harryyan1238@gmail.com>
2021-12-05 08:18:41 +1300

    leetcode 151

---
+++ 分类/数组-字符串-链表/leetcode_151-reverse-words-I.py
@@ -0,0 +1,27 @@
+# Given an input string s, reverse the order of the words.
+
+# A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+# Return a string of the words in reverse order concatenated by a single space.
+
+# Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip() # 删除首尾空格
+        i = j = len(s) - 1
+        res = []
+        while i >= 0:
+            while i >= 0 and s[i] != ' ': i -= 1 # 搜索首个空格
+            res.append(s[i + 1: j + 1]) # 添加单词
+            while s[i] == ' ': i -= 1 # 跳过单词间空格
+            j = i # j 指向下个单词的尾字符
+
+        return ' '.join(res) # 拼接并返回
+
+
+s = "the sky is blue"
+so = Solution()
+
+r = so.reverseWords(s)
+print(r)
