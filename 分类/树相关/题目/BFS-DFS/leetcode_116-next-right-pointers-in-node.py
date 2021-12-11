dad6b9f28895143fda72870dbc360c16ec2b0c42
HarryYan <harryyan1238@gmail.com>
2021-12-11 21:53:23 +1300

    add quick sort swift version

--- 分类/树相关/题目/BFS-DFS/leetcode_116-next-right-pointers-in-node.py
+++ 分类/树相关/题目/BFS-DFS/leetcode_116-next-right-pointers-in-node.py
@@ -1,12 +1,17 @@
 # You are given a perfect binary tree where all leaves are on the same level,
 # and every parent has two children. The binary tree has the following definition:
 
-# struct Node {
-#   int val;
-#   Node *left;
-#   Node *right;
-#   Node *next;
-# }
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
 # Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
 
 # Initially, all next pointers are set to NULL.
