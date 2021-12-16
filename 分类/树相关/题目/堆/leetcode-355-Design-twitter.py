9ae66957c718b7595cfc35db7faa47f672cf4ada
HarryYan <harryyan1238@gmail.com>
2021-12-16 17:26:32 +1300

    leetcode 355

---
+++ 分类/树相关/题目/堆/leetcode-355-Design-twitter.py
@@ -0,0 +1,55 @@
+# Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, 
+# and is able to see the 10 most recent tweets in the user's news feed.
+
+# Implement the Twitter class:
+
+# Twitter() Initializes your twitter object.
+# void postTweet(int userId, int tweetId) Composes a new tweet with ID tweetId by the user userId. Each call to this function will be made with a unique tweetId.
+# List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent tweet IDs in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user themself. Tweets must be ordered from most recent to least recent.
+# void follow(int followerId, int followeeId) The user with ID followerId started following the user with ID followeeId.
+# void unfollow(int followerId, int followeeId) The user with ID followerId started unfollowing the user with ID followeeId.
+from typing import defaultdict, list
+
+class Twitter:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.tweets = defaultdict(list)
+        self.tweets_time = defaultdict(list)
+        self.time = 0
+        self.follows = defaultdict(set)
+    
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        """
+        Compose a new tweet.
+        """
+        self.tweets[userId].insert(0, tweetId)
+        self.tweets_time[userId].append([self.time, tweetId])
+        self.time+=1
+
+    def getNewsFeed(self, userId: int) -> list:
+        """
+        Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+        """
+        news = self.tweets_time[userId].copy()
+        for f in self.follows[userId]:
+            news+=self.tweets_time[f]
+        if news: 
+            news.sort(key=lambda x:x[0], reverse=True)
+            return [t[1] for t in news][0:10]
+        else: 
+            return []
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        """
+        Follower follows a followee. If the operation is invalid, it should be a no-op.
+        """
+        self.follows[followerId].add(followeeId)
+        
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        """
+        Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+        """
+        if followeeId in self.follows[followerId]: 
+            self.follows[followerId].remove(followeeId)
