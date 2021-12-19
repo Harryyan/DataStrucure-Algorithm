96685979b17809c6bfc86551ebd2bc67cd209fc1
HarryYan <harryyan1238@gmail.com>
2021-12-19 20:10:52 +1300

    leetcode 7- reverse integer

---
+++ 分类/位操作/leeetcode_7-reverse-integer.py
@@ -0,0 +1,24 @@
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+class Solution:
+    def reverse(
+        self, 
+        x: int) -> int:
+       
+        
+        y, res = abs(x), 0
+        
+        # 则其数值范围为 [−2^31,  2^31 − 1]
+        boundry = (1<<31) -1 if x>0 else 1<<31
+        while y != 0:
+            res = res*10 +y%10
+            if res > boundry :
+                return 0
+            y //=10
+        return res if x >0 else -res
+
+
+
+        
