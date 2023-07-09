50757d3d9b44c4617e8e7b419dcdb654ae07ca09
Harry Yan <harryyan1238@gmail.com>
2023-07-09 19:59:02 +1200

    update lc-269

--- 算法分类/图/题目/leetcode_269-Alien-Dictionary.py
+++ 算法分类/图/题目/leetcode_269-Alien-Dictionary.py
@@ -1,12 +1,33 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+from itertools import pairwise
 from typing import List
 
-# There is a new alien language that uses the English alphabet. However, the order among the letters is unknown to you.
-# You are given a list of strings words from the alien language's dictionary, where the strings in words are sorted lexicographically by the rules of this new language.
-# Return a string of the unique letters in the new alien language sorted in lexicographically increasing order by the new language's rules. If there is no solution, return "". If there are multiple solutions, return any of them.
-# A string s is lexicographically smaller than a string t if at the first letter where they differ, the letter in s comes before the letter in t in the alien language.
-#  If the first min(s.length, t.length) letters are the same, then s is smaller if and only if s.length < t.length.
+class Solution2:
+    def alienOrder(self, words: List[str]) -> str:
+        g = defaultdict(list)
+        inDeg = { c:0 for c in words[0] }   # 入度个数记录
 
+        for s, t in pairwise(words):   # 字母序只能按照相邻的词比较
+            for c in t:
+                inDeg.setdefault(c,0)
+
+            for u,v in zip(s,t):
+                if u != v:
+                    g[u].append(v)
+                    inDeg[v] += 1
+                    break    # 无需再比较
+            else:
+                if len(s) > len(t):   # 形如["abc", "ab"]的样例是违反定义的, 特殊判断
+                    return ""     
+                
+        q = [u for u, d in inDeg.items() if d == 0]
+
+        for u in q:
+            for v in g[u]:  # 出度图
+                inDeg[v] -= 1
+                if inDeg[v] == 0: q.append(v)
+        
+        return ''.join(q) if len(q) == len(inDeg) else ""
 
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
