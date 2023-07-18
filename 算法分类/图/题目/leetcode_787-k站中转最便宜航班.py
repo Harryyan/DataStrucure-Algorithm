c6f3cf16d16a22c1036a33d898502aee067045aa
Harry Yan <harryyan1238@gmail.com>
2023-07-18 16:27:35 +1200

    update

--- 算法分类/图/题目/leetcode_787-k站中转最便宜航班.py
+++ 算法分类/图/题目/leetcode_787-k站中转最便宜航班.py
@@ -1,12 +1,6 @@
 from typing import List
 import collections
 
-# There are n cities connected by some number of flights. You are given an array flights where 
-# flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
-# You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. 
-# If there is no such route, return -1.
-# There will not be any multiple flights between two cities.
-
 # leetcode - 787
 # SPFA
 
